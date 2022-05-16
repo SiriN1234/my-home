574d5fc5a0210790242bc68e3c7a5345fc3b547c
unknown <SiriN1234>
2022-05-16 16:24:16 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -5,3 +5,26 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+# 카카오톡 텍스트형 응답
+@app.route('/api/test', methods=['POST'])
+def test():
+    body = request.get_json() # 사용자가 입력한 데이터
+    print(body)
+    print(body['userRequest']['utterance'])
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "안녕하세요? 테스트 메세지입니다."
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
