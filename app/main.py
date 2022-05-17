8ecfbe81ecc3b732e02ab602285ac172c90a432f
unknown <SiriN1234>
2022-05-17 09:47:43 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -29,3 +29,51 @@ def test():
     }
 
     return responseBody
+
+
+# 카카오톡 카드 테스트
+@app.route('/api/cardTest', methods=['POST'])
+def cardTest():
+    body = request.get_json() # 사용자가 입력한 데이터
+    print(body)
+    print(body['userRequest']['utterance'])
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+        "outputs": [
+        {
+            "basicCard": {
+                "title": "보물상자",
+                "description": "보물상자 안에는 뭐가 있을까",
+                "thumbnail": {
+                    "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
+                },
+                "profile": {
+                    "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4BJ9LU4Ikr_EvZLmijfcjzQKMRCJ2bO3A8SVKNuQ78zu2KOqM",
+                    "nickname": "보물상자"
+                },
+                "social": {
+                    "like": 1238,
+                    "comment": 8,
+                    "share": 780
+                },
+                "buttons": [
+                {
+                    "action": "message",
+                    "label": "열어보기",
+                    "messageText": "짜잔! 우리가 찾던 보물입니다"
+                },
+                {
+                    "action":  "webLink",
+                    "label": "구경하기",
+                    "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
+                }
+                ]
+            }
+        }
+        ]
+    }
+    }
+
+    return responseBody
