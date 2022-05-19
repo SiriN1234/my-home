59d4d31496753e9604e741111b7714cb6bf633ae
unknown <SiriN1234>
2022-05-19 16:00:52 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -62,7 +62,7 @@ def test1():
     return responseBody
 
 
-# 다른 파일에서 값 가져오기 테스트
+# 다른 파이썬 파일에서 함수 호출 연습
 @app.route('/api/test2', methods=['POST'])
 def test2():
     body = request.get_json() # 사용자가 입력한 데이터
@@ -81,6 +81,56 @@ def test2():
             ]
         }
     }
+
+    return responseBody
+
+
+
+
+# 보물상자
+@app.route('/api/test3', methods=['POST'])
+def test3():
+    body = request.get_json() # 사용자가 입력한 데이터
+    print(body)
+    print(body['userRequest']['utterance'])
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+        "outputs": [
+        {
+            "basicCard": {
+            "title": "보물상자",
+            "description": "보물상자 안에는 뭐가 있을까",
+            "thumbnail": {
+                "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
+            },
+            "profile": {
+                "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4BJ9LU4Ikr_EvZLmijfcjzQKMRCJ2bO3A8SVKNuQ78zu2KOqM",
+                "nickname": "보물상자"
+            },
+            "social": {
+                "like": 1238,
+                "comment": 8,
+                "share": 780
+            },
+            "buttons": [
+            {
+                "action": "message",
+                "label": "열어보기",
+                "messageText": "짜잔! 우리가 찾던 보물입니다"
+            },
+            {
+                "action":  "webLink",
+                "label": "구경하기",
+                "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
+            }
+          ]
+        }
+      }
+    ]
+  }
+}
 
     return responseBody
 
