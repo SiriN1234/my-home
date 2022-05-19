8d59477ea054814994c4551b4f301569965dbb9b
unknown <SiriN1234>
2022-05-19 16:21:59 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -100,29 +100,20 @@ def test3():
         "outputs": [
         {
             "basicCard": {
-            "title": "보물상자",
-            "description": "보물상자 안에는 뭐가 있을까",
+            "title": "타이틀",
+            "description": "왼쪽을 누르면 텍스트를 반환, 오른쪽을 누르면 링크 연결",
             "thumbnail": {
                 "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
-            },
-            "profile": {
-                "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4BJ9LU4Ikr_EvZLmijfcjzQKMRCJ2bO3A8SVKNuQ78zu2KOqM",
-                "nickname": "보물상자"
-            },
-            "social": {
-                "like": 1238,
-                "comment": 8,
-                "share": 780
             },
             "buttons": [
             {
                 "action": "block",
-                "label": "열어보기",
+                "label": "텍스트",
                 "blockId": "6285ee1475eca02fba63ce07?scenarioId=627b131e9ac8ed7844165d72"
             },
             {
                 "action":  "webLink",
-                "label": "구경하기",
+                "label": "링크",
                 "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
             }
           ]
