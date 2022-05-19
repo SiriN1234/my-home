2fab7e8a7ca335c1c89a422bf5585dafd5a35a35
unknown <SiriN1234>
2022-05-19 10:17:46 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -41,15 +41,13 @@ def test1():
     print(body)
     print(body['userRequest']['utterance'])
 
-    a = test1()
-
     responseBody = {
         "version": "2.0",
         "template": {
             "outputs": [
                 {
                     "simpleText": {
-                        "text": a
+                        "text": test1()
                     }
                 }
             ]
