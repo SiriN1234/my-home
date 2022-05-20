e21ba263e42754e5899c41a4d8c9f4ca91b4a12a
unknown <SiriN1234>
2022-05-20 12:18:29 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -171,7 +171,7 @@ def cardTest():
                                 "action":  "webLink",
                                 "label": "자세히 보기",
                                 "pcUrl": "https://www.myhome.go.kr/hws/portal/sch/selectRsdtRcritNtcDetailView.do?pblancId=11335",
-                                "mobileUrl" : "https://www.naver.com"
+                                "moUrl" : "https://m.myhome.go.kr/hws/mbl/sch/selectRsdtLttotListView.do#detailPage?pblancId=11440&searchSe=R"
                             }
                         ]
                     },
