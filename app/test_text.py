a788d8a10cc5c9d5679b0eca0397b3de0b3bc62c
unknown <SiriN1234>
2022-05-24 14:21:10 +0900

    updated

--- app/test_text.py
+++ app/test_text.py
@@ -1,20 +1,3 @@
-import random
-import pandas as pd
-import csv
-
 def pt() :
     a = "abcde"
     return a
-
-thumbnail = pd.read_csv("./data/Thumbnail.csv")
-thumbnail_arr = []
-for i in range(10) :
-    thumbnail_arr.append(thumbnail.iloc[i]['thumbnail'])
-    
-rand_thumb = random.sample(thumbnail_arr, 10)
-
-print(rand_thumb[0])
-
-rand_thumb = random.sample(thumbnail_arr, 10)
-
-print(rand_thumb[0])
