7abff6c19efa64082b56c9b736c9e53faae3180e
unknown <hanjongin1@naver.com>
2022-05-26 15:15:44 +0900

    updated

--- app/bl_test.py
+++ app/bl_test.py
@@ -3,11 +3,11 @@ from flask import Flask, request, jsonify
 import pandas as pd
 
 # ----- house_welfare blueprint set ----------------------------------------------------------------------------
-blue_test = Blueprint("blue_test", __name__)
+app = Blueprint("blue_test", __name__)
 #---------------------------------------------------------------------------------------------------------------
 
 # ----- house_welfare root -------------------------------------------------------------------------------------
-@blue_test.route("/api/blue_test")
+@app.route("/api/blue_test")
 def house_welfare_home():
     return "house_welfare"
 # --------------------------------------------------------------------------------------------------------------
